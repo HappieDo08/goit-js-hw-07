25c4e6031687ef98dd15f17031edad2e98c99832
HappieDo08 <aquinogerardoXIII@gmail.com>
2024-02-08 19:01:18 +0800

    initial commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+
+const createGallery = element => {
+    return element
+        .map(({ preview, original, description }) => {
+            return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    date-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>
+            `
+        })
+        .join('');
+}
+
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photosMarkup);
+
+
+const handleGalleryClick = event => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const urlOriginal = event.target.dataset.source;
+
+    const instance = basicLightbox.create(` <img src="${urlOriginal}">`);
+    instance.show();
+};
+
 console.log(galleryItems);
